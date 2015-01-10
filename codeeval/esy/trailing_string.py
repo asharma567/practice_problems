2e0f71e0c3c45ff7514ae8e869f8469823668ac4
Ajay Sharma <asharma567567@gmail.com>
2015-01-10 15:14:53 -0800

    commiting this..

--- codeeval/esy/trailing_string.py
+++ codeeval/esy/trailing_string.py
@@ -5,10 +5,10 @@ import sys
 
 def check_this_line(line_str):
     subj_line, end = line_str.strip().split(',')
-    return int(' '.join(subj_line.split()[-len(end.split()):]) == end)
+    return int(subj_line[-len(end):] == end)
 
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as f:
         for line in f:
-            print check_this_line(line)
+            print check_this_line(line), line
