6e7c4ee7958f8c2c8353f2675294ad310522f448
Ajay Sharma <asharma567567@gmail.com>
2015-01-17 19:27:31 -0800

    skipping

--- codeeval/esy/query_board.py
+++
@@ -1,6 +0,0 @@
-'''
-https://www.codeeval.com/open_challenges/87/submit/?lid=758502
-
-I want to do redo this one using Numpy
-'''
-
--- codeeval/esy/sum_of_integers.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as f:
-
-        #leaving the last element off because of blank at the end
-        input_lis_int_str = f.read().split('\n')[:-1]
-    print sum(map(int, input_lis_int_str))
