abc6d570087922698e43227cec25294cd4f5647b
Ajay Sharma <asharma@AJSharma-MBP.local>
2015-09-10 12:06:50 -0700

    solved a tough problem

--- codeeval/difficult/palindromic_ranges.py
+++ codeeval/difficult/palindromic_ranges.py
@@ -29,17 +29,26 @@ def find_num_of_palindromes(range_):
     return sum([is_palindrome(x) for x in range_])
 
 def find_all_palies(L, R):
+    if L == R: return is_palindrome(L)
+        
     range_of_ints = range(L, R + 1)
+    
     lis_of_pali = [find_n_grams(range_of_ints, i) for i in range(len(range_of_ints))]
-    count_of_even_palies = len([find_num_of_palindromes(tup) for sub_list in lis_of_pali for tup in sub_list if find_num_of_palindromes(tup) % 2 == 0]) 
-    if count_of_even_palies in (0,1): 
-        x = len([find_num_of_palindromes(tup) for sub_list in lis_of_pali for tup in sub_list])
+    
+    list_of_even_palies = [find_num_of_palindromes(tup) for sub_list in lis_of_pali for tup in sub_list if find_num_of_palindromes(tup) % 2 == 0]
+    
+    #filter out 0s
+    count_of_even_palies = len([x for x in list_of_even_palies if x != 0])
+    
+    if count_of_even_palies in (0,1):
+        x = len([find_num_of_palindromes(tup) for sub_list in lis_of_pali for tup in sub_list if find_num_of_palindromes(tup) != 0])        
         return 1 if x > 0 else 0
-    return len([find_num_of_palindromes(tup) for sub_list in lis_of_pali for tup in sub_list])
+    return count_of_even_palies
 
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as f:
         for line in f:
-            a, b = line.split()
+            if line == '': continue
+            a, b = line.strip().split()
             print find_all_palies(int(a), int(b))
